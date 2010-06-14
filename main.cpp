22bbdcdaa1ef9c2a418042acac59e4548141734c
Kashif Kaleem <kashifkaleem1@gmail.com>
2010-06-14 22:35:14 +0100

    basic cuda integration

--- main.cpp
+++ main.cpp
@@ -1,48 +1,15 @@
-/*****************************************************************************
- *   Copyright (C) 2004-2009 The PaGMO development team,                     *
- *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
- *   http://apps.sourceforge.net/mediawiki/pagmo                             *
- *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
- *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
- *   act@esa.int                                                             *
- *                                                                           *
- *   This program is free software; you can redistribute it and/or modify    *
- *   it under the terms of the GNU General Public License as published by    *
- *   the Free Software Foundation; either version 2 of the License, or       *
- *   (at your option) any later version.                                     *
- *                                                                           *
- *   This program is distributed in the hope that it will be useful,         *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
- *   GNU General Public License for more details.                            *
- *                                                                           *
- *   You should have received a copy of the GNU General Public License       *
- *   along with this program; if not, write to the                           *
- *   Free Software Foundation, Inc.,                                         *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
- *****************************************************************************/
 
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <boost/algorithm/string.hpp>
-#include <boost/lexical_cast.hpp>
-#include"src/pagmo.h"
+#include <stdio.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
 
-using namespace pagmo;
-using namespace kep_toolbox;
+#include "src/ann_toolbox/cuda/external_dependency.h"
 
-int main()
+extern int doit();
+
+int main( int argc, char **argv )
 {
+  CHECK_CUDA_ERROR();
+  return doit();
+};
 
-	algorithm::sa_corana algo(10000,10,0.001);
-	problem::levy5 prob(10);
-	archipelago a(prob,algo,2,20,::pagmo::topology::ring());
-	std::cout << algo << std::endl;
-	std::cout << "To solve:" << '\n' << prob << std::endl;
-	for (int i = 0; i<200; ++i) {
-		a.evolve();
-		std::cout << a.get_island(0).get_population().champion().f << std::endl;
-	}
-	return 0;
-}
