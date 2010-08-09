e0026202958d3dc02aa2713e8b15279727704c73
Edgar Simo <bobbens@gmail.com>
2010-08-09 11:02:07 +0200

    * Added support for loading CSV files directly for data.

--- PyGMO/visualization/__init__.py
+++ PyGMO/visualization/__init__.py
@@ -30,6 +30,8 @@ import traj3d
 import traj3d_traj
 import traj3d_planet
 
+import csv
+
 
 class Trajectory3D:
    """
@@ -40,6 +42,16 @@ class Trajectory3D:
       """
       Constructor for the Trajectory3D.
       """
+      # Handle data
+      if type(data).__name__ == 'str':
+         data_csv = csv.reader( open(data, 'r') )
+         data     = []
+         for row in data_csv:
+            data.extend( row )
+         data     = map( lambda x: float(x), data )
+         fp       = None
+
+      # Defaults
       self.__axes    = None
       self.__origin  = None
       self.__data    = data
@@ -149,33 +161,13 @@ class Trajectory3D:
 
 # Run some tests
 if __name__ == "__main__":
-   data = ( 0.,
-               135299631.153314,
-                  -67433330.1118738,
-                  0.,
-               13.3117666043686,
-                  30.2608967735421,
-                  -1.21905509181638,
-               0.,
-                  0.,
-                  0.,
-            350.0,
-               -242539545.423682,
-                  -36777066.1865885,
-                  5209166.99174208,
-               5.46701901228146,
-                  -19.7530014959265,
-                  0.562626144359047,
-               0.,
-                  0.,
-                  0. )
-   mjd2000 = (8273.26728762578, 8623.26728762578)
-   planets = { 3 : (1.,0.,0.), 4: (0.,1.,0.) }
+   #mjd2000 = (8273.26728762578, 8623.26728762578)
+   #planets = { 3 : (1.,0.,0.), 4: (0.,1.,0.) }
 
    # Create the engine
-   traj = Trajectory3D( data, 640, 480,
+   traj = Trajectory3D( "CassiniMGA.txt", 640, 480,
          24.*3600., 1000., 1000., 1. ) # Unit conversions: days->s, km->m
-   traj.addPlanets( mjd2000, planets )
+   #traj.addPlanets( mjd2000, planets )
    traj.setUnits( "km", "d", "km/s" )
 
    # Create some stuff
