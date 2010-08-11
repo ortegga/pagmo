f8138485066d23127cf2b7dae54b0caffff9c9b4
Edgar Simo <bobbens@gmail.com>
2010-08-11 13:17:24 +0200

    * Added function to get planet name.

--- PyGMO/visualization/traj3d_planet.py
+++ PyGMO/visualization/traj3d_planet.py
@@ -86,6 +86,9 @@ class Planet(Object):
       # Create path
       self.__path    = Path( data, 24.*3600., 1000., 1000., 1., mu, info['colour'] )
 
+   def name( self ):
+      return self.__info['name']
+
    def setFont( self, font, size ):
       self.__font       = font
       self.__fontsize   = size
